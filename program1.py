d610b7b60ad992c337a39a4ea4b0705a2e0e7fa2
Meet Jain <imeetjain07@outlook.com>
2024-10-08 15:42:37 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,8 +1,20 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
+        class Solution:
+  def isValid(self, s: str) -> bool:
+    stack = []
+
+    for c in s:
+      if c == '(':
+        stack.append(')')
+      elif c == '{':
+        stack.append('}')
+      elif c == '[':
+        stack.append(']')
+      elif not stack or stack.pop() != c:
+        return False
+
+    return not stack
         """
         pass
 
--- test1.py
+++ test1.py
@@ -1,18 +1,3 @@
 import unittest
 from program1 import Solution
 
-class Solution:
-  def isValid(self, s: str) -> bool:
-    stack = []
-
-    for c in s:
-      if c == '(':
-        stack.append(')')
-      elif c == '{':
-        stack.append('}')
-      elif c == '[':
-        stack.append(']')
-      elif not stack or stack.pop() != c:
-        return False
-
-    return not stack
